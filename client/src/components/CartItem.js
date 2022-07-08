099d5042a806971e0834f20a41202ad57b5ffdca
FungusGaming <fungus0922@gmail.com>
2022-07-08 16:35:02 +0800

    Add remove item from cart list

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,11 +1,24 @@
-const CartItem = ({ item }) => {
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../features/cart/cartSlice";
+import SampleImg from '../assets/img/SampleImage.png'
+
+const CartItem = ({ item, index }) => {
   const { name, price } = item
+  const dispatch = useDispatch();
   return (
-    <div id='cart-item' className="flex-row align-center space-between p-l">
-      <div className="font-bold">
-        {name}
+    <div id='cart-item' className="flex-row align-center">
+      <div className="flex-row align-center space-between p-l flex-one">
+        <div className="font-bold flex-row align-center">
+          <img id='product-img' className='thumbnail-img mr-l' src={SampleImg} alt='product img' />
+          <span>{name}</span>
+        </div>
+        <div>{price}</div>
       </div>
-      <div>{price}</div>
+      <div className="pointer p-m color-red" onClick={() => dispatch(removeFromCart(index))}>
+        <FontAwesomeIcon icon={faMinus} />
+      </div>
     </div>
   )
 }
