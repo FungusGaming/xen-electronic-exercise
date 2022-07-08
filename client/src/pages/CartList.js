7fcc8eb605782ee3b4f359655ff6e3efff49a9a2
FungusGaming <fungus0922@gmail.com>
2022-07-08 16:35:02 +0800

    Add remove item from cart list

--- client/src/pages/CartList.js
+++ client/src/pages/CartList.js
@@ -1,7 +1,7 @@
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import CartItem from "../components/CartItem"
 import { cartItems } from "../features/cart/cartSlice"
 import Footer from "../layout/footer"
@@ -21,25 +21,26 @@ const CartList = () => {
     <div className="flex-column flex-one">
       <Header />
       <div className="flex-one p-l border-solid">
-        <div id='cart-list-title' className='flex-row mb-l'>
+        <div id='cart-list-title' className='flex-row mb-l align-center'>
           <button className="trans-button" onClick={() => navigate(-1)}>
             <FontAwesomeIcon icon={faArrowLeft} />
           </button>
           <h1>Your Cart</h1>
         </div>
         {
-          items.length === 0 ? <div>
-            Your cart is empty
+          items.length === 0 ? <div id="empty-cart-list">
+            <span className="mr-l">Your cart is empty</span>
+            <Link className="primary-button" to="/">Shop something</Link>
           </div> : <div id='cart-list-item'>
-            {items.map(item => (
-              <CartItem item={item} />
+            {items.map((item, index) => (
+              <CartItem item={item} index={index} />
             ))}
-            <div className="flex-row align-center space-between p-l">
-              <div>Total:</div>
+            <div className="flex-row align-center justify-end p-l mr-xl">
+              <div>Total:&nbsp;</div>
               <div className="color-currency font-bold">{getTotal()}</div>
             </div>
-            <div className="flex-row justify-end">
-              <button>Checkout</button>
+            <div className="flex-row justify-end align-center mt-l">
+              <button className="primary-button" disabled>Checkout</button>
             </div>
           </div>
         }
