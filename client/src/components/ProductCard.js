1380767f018ad8a7541e78cdd7e5dd2344373435
FungusGaming <fungus0922@gmail.com>
2022-08-15 23:01:12 +0800

    MODIFY logic for ctaMessage

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,14 +1,19 @@
 import { faAdd } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useCallback } from 'react'
+import { useDispatch  } from 'react-redux';
 import AppConfig from '../app/config'
 import SampleImg from '../assets/img/SampleImage.png'
 import { addToCart } from '../features/cart/cartSlice';
-
+import { setMessage } from '../features/message/messageSlice';
 const ProductCard = ({ className, product }) => {
   const { name, price } = product
   const dispatch = useDispatch();
+  const addItemToCart = useCallback(() => {
+    dispatch(addToCart(product))
+    dispatch(setMessage('Added to cart!'))
+    
+  }, [dispatch, product])
   return (
     <div id='product-card' className={`product-card ${className}`}>
       <img id='product-img' className='product-img' src={SampleImg} alt='product img' />
@@ -22,7 +27,7 @@ const ProductCard = ({ className, product }) => {
             {price && <p>{AppConfig[process.env.REACT_APP_REGION].currency}{price}</p>}
           </div>
         </div>
-        <div className='pointer' onClick={() => dispatch(addToCart(product))}>
+        <div className='pointer' onClick={addItemToCart}>
           <FontAwesomeIcon icon={faAdd} />
         </div>
       </div>
