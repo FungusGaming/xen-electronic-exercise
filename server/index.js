f73e419476ca0d31a8e104ae23f3140aee0663c2
FungusGaming <fungus0922@gmail.com>
2022-09-06 22:39:18 +0800

    Add express-session

--- server/index.js
+++ server/index.js
@@ -2,15 +2,22 @@ const express = require('express');
 const app = express()
 const path = require('path');
 const cors = require('cors');
+const session = require('express-session')
 
 const productRouter = require('./model/Product')
 
+const sessionOptions = {
+  secret: 'notgoodsecret',
+  resave: false,
+  saveUnintialized: false
+}
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/product', productRouter)
+app.use(session(sessionOptions))
 
 // error handler
 app.use((err, req, res, next) => {
