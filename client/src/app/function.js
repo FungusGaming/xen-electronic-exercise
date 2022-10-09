1fe82a12af78ec45ae03b0705b78ab544027617a
Wei Fung <fungus0922@gmail.com>
2022-10-09 22:35:13 +0800

    Remove log and fix message status

--- client/src/app/function.js
+++ client/src/app/function.js
@@ -1,9 +1,7 @@
 export const handleAxiosError = (err) => {
-  console.error('Axios err:', err);
   let message = 'Unexpected error!'
   if (err.response.data && err.response.data.status && err.response.data.message) {
     message = err.response.data.message
-
   }
   return message
 }
