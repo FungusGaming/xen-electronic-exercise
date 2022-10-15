db0ad5a704a2600da67c78e55efe6ab0460a0b5c
Wei Fung <fungus0922@gmail.com>
2022-10-15 16:05:52 +0800

    Admin role control
    Client
    - Add userReducer
    - Admin navigation on header

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import cartReducer from '../features/cart/cartSlice';
 import messageReducer from '../features/message/messageSlice';
+import userReducer from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     cart: cartReducer,
-    message: messageReducer
+    message: messageReducer,
+    user: userReducer
   },
 });
--- client/src/layout/header.js
+++ client/src/layout/header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faToolbox, faUser } from '@fortawesome/free-solid-svg-icons'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { cartCount } from '../features/cart/cartSlice'
+import { userRole } from '../features/user/userSlice'
 const Header = () => {
   const count = useSelector(cartCount)
+  const role = useSelector(userRole)
   return (
     <div id='header' className='flex-row space-between align-center'>
       <section className='p-l pointer'>XenElectronic</section>
       <section className='flex-row mr-l'>
+        { role && role === '1' && <Link to={'/admin'} className='pr-l'>
+          <FontAwesomeIcon icon={faToolbox} />
+        </Link>}
         <Link to={'/signin'} className='pr-l'>
           <FontAwesomeIcon icon={faUser} />
         </Link>
